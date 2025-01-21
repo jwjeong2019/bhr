2d10279b745dfc6cda441cde849e04304fd5aa55
jwjeong <timestar7979@gmail.com>
2025-01-21 23:58:54 +0900

    feat: 부서 관리 컨트롤러 수정
    - 화면 컨트롤러 수정
    - Update 컨트롤러 구현

--- erp-spring/src/main/java/com/erp/controller/HRDepController.java
+++ erp-spring/src/main/java/com/erp/controller/HRDepController.java
@@ -1,14 +1,60 @@
 package com.erp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.erp.dto.CriterionDto;
 
 @Controller
 public class HRDepController {
 	
+	public List<CriterionDto> list = new ArrayList<>();
+	
+	public HRDepController() {
+		list.add(new CriterionDto("A0005", "DEVELOP", "Y", "개발2팀", "N"));
+		list.add(new CriterionDto("A0006", "BUSINESS", "Y", "영업1팀", "N"));
+		list.add(new CriterionDto("A0001", "MANAGE", "Y", "경영팀", "Y"));
+		list.add(new CriterionDto("A0002", "HR", "Y", "인사팀", "Y"));
+		list.add(new CriterionDto("A0003", "DEVELOP", "Y", "개발1팀", "Y"));
+		list.add(new CriterionDto("A0004", "BUSINESS", "Y", "영업2팀", "Y"));
+	}
+	
 	@RequestMapping("/hrDepartment.do")
-	public String hrDepartment() {
+	public String hrDepartment(Model model) {
+		List<CriterionDto> listArrN = list.stream()
+				.filter(dto -> dto.getArrangement().equals("N"))
+				.collect(Collectors.toList());
+		List<CriterionDto> listArrY = list.stream()
+				.filter(dto -> dto.getArrangement().equals("Y"))
+				.collect(Collectors.toList());
+		
+		model.addAttribute("listArrN", listArrN);
+		model.addAttribute("listArrY", listArrY);
 		return "hr_department";
+	}
+	
+	@PostMapping("/hrDepartmentUpdate.do")
+	public String hrDepartmentUpdate(HttpServletRequest request) {
+		String code = request.getParameter("code");
+		String arrangement = request.getParameter("arrangement");
+		
+		list = list.stream().map(dto -> {
+			if (dto.getCode().equals(code)) {
+				dto.setArrangement(arrangement);
+				return dto;
+			}
+			return dto;
+		}).collect(Collectors.toList());
+		
+		return "redirect:/hrDepartment.do";
 	}
 	
 }
