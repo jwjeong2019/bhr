453acd67ef7c1db2e0239f44f7ff9afd4a275665
jwjeong <timestar7979@gmail.com>
2025-01-23 03:26:00 +0900

    feat: SampleController 로그 확인

--- erp-spring/src/main/java/com/erp/controller/SampleController.java
+++ erp-spring/src/main/java/com/erp/controller/SampleController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +16,17 @@ import com.erp.vo.SampleVO;
 @Controller
 public class SampleController {
 	
+	private static final Logger logger = LogManager.getLogger(SampleController.class);
+	
 	@Inject
 	private SampleService service;
 	
 	@GetMapping("/sample")
 	public String sample(Model model) throws Exception {
+		
+		logger.info("hello SampleController.");
+		logger.warn("hello SampleController.");
+		logger.debug("hello SampleController.");
 		
 		List<SampleVO> sampleList = service.getSampleList();
 		
