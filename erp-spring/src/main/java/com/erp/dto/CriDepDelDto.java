0d61cc75825811a592e6720a1d21a27b2608e4e0
jwjeong <timestar7979@gmail.com>
2025-01-23 23:46:58 +0900

    feat: DTO 클래스 추가

--- erp-spring/src/main/java/com/erp/dto/CriDepDelDto.java
+++ erp-spring/src/main/java/com/erp/dto/CriDepDelDto.java
@@ -1,5 +1,31 @@
 package com.erp.dto;
 
 public class CriDepDelDto {
+	
+	private String reqCode;
+	
+	private boolean isSuccess;
+	private String resRedirectUrl;
+	
+	public String getReqCode() {
+		return reqCode;
+	}
+	public void setReqCode(String reqCode) {
+		this.reqCode = reqCode;
+	}
+	public boolean isSuccess() {
+		return isSuccess;
+	}
+	public void setSuccess(boolean isSuccess) {
+		this.isSuccess = isSuccess;
+	}
+	public String getResRedirectUrl() {
+		return resRedirectUrl;
+	}
+	public void setResRedirectUrl(String resRedirectUrl) {
+		this.resRedirectUrl = resRedirectUrl;
+	}
+	
+	
 
 }
--- erp-spring/src/main/java/com/erp/dto/CriDepDto.java
+++ erp-spring/src/main/java/com/erp/dto/CriDepDto.java
@@ -1,5 +1,17 @@
 package com.erp.dto;
 
+import java.util.List;
+
 public class CriDepDto {
+	
+	private List<CriterionDto> resList;
+
+	public List<CriterionDto> getResList() {
+		return resList;
+	}
+
+	public void setResList(List<CriterionDto> resList) {
+		this.resList = resList;
+	}
 
 }
