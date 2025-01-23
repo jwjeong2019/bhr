e0c3b33b02cca8caf4b475326133b687ea94a7cc
jwjeong <timestar7979@gmail.com>
2025-01-23 23:51:17 +0900

    feat: DAO 수정

--- erp-spring/src/main/java/com/erp/dao/CriterionDao.java
+++ erp-spring/src/main/java/com/erp/dao/CriterionDao.java
@@ -2,10 +2,15 @@ package com.erp.dao;
 
 import java.util.List;
 
+import com.erp.dto.CriterionDto;
 import com.erp.vo.CriterionVO;
 
 public interface CriterionDao {
 	
-	public List<CriterionVO> selectCriterion() throws Exception;
+	public CriterionVO selectOne(CriterionDto dto) throws Exception;
+	public List<CriterionVO> selectList(CriterionDto dto) throws Exception;
+	public int insert(CriterionDto dto) throws Exception;
+	public int update(CriterionDto dto) throws Exception;
+	public int delete(CriterionDto dto) throws Exception;
 
 }
--- erp-spring/src/main/java/com/erp/dao/CriterionDaoImpl.java
+++ erp-spring/src/main/java/com/erp/dao/CriterionDaoImpl.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import com.erp.dto.CriterionDto;
 import com.erp.vo.CriterionVO;
 
 @Repository
@@ -18,9 +19,33 @@ public class CriterionDaoImpl implements CriterionDao {
 	private SqlSession sqlSession;
 
 	@Override
-	public List<CriterionVO> selectCriterion() throws Exception {
+	public CriterionVO selectOne(CriterionDto dto) throws Exception {
 		// TODO Auto-generated method stub
-		return sqlSession.selectList(NAMESPACE + ".selectCriterion");
+		return sqlSession.selectOne(NAMESPACE + ".selectCriterion", dto);
+	}
+
+	@Override
+	public List<CriterionVO> selectList(CriterionDto dto) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.selectList(NAMESPACE + ".selectCriterion", dto);
+	}
+
+	@Override
+	public int insert(CriterionDto dto) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.insert(NAMESPACE + ".insertCriterion", dto);
+	}
+
+	@Override
+	public int update(CriterionDto dto) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.update(NAMESPACE + ".updateCriterion", dto);
+	}
+
+	@Override
+	public int delete(CriterionDto dto) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.delete(NAMESPACE + ".deleteCriterion", dto);
 	}
 
 }
