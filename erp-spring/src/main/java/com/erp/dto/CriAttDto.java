06379a01f17cc0acfe9badd7193937d3dfd9fc34
jwjeong <timestar7979@gmail.com>
2025-01-24 00:36:45 +0900

    feat: DTO 일괄 수정

--- erp-spring/src/main/java/com/erp/dto/CriAttDto.java
+++ erp-spring/src/main/java/com/erp/dto/CriAttDto.java
@@ -1,5 +1,17 @@
 package com.erp.dto;
 
+import java.util.List;
+
 public class CriAttDto {
+	
+	private List<CriterionDto> resList;
+
+	public List<CriterionDto> getResList() {
+		return resList;
+	}
+
+	public void setResList(List<CriterionDto> resList) {
+		this.resList = resList;
+	}
 
 }
--- erp-spring/src/main/java/com/erp/dto/CriAttUpdDto.java
+++ erp-spring/src/main/java/com/erp/dto/CriAttUpdDto.java
@@ -1,5 +1,50 @@
 package com.erp.dto;
 
 public class CriAttUpdDto {
+	
+	private String reqCode;
+	private String reqType;
+	private char reqStatus;
+	private String reqName;
+	
+	private boolean isSuccess;
+	private String resRedirectUrl;
+	
+	public String getReqCode() {
+		return reqCode;
+	}
+	public void setReqCode(String reqCode) {
+		this.reqCode = reqCode;
+	}
+	public String getReqType() {
+		return reqType;
+	}
+	public void setReqType(String reqType) {
+		this.reqType = reqType;
+	}
+	public char getReqStatus() {
+		return reqStatus;
+	}
+	public void setReqStatus(char reqStatus) {
+		this.reqStatus = reqStatus;
+	}
+	public String getReqName() {
+		return reqName;
+	}
+	public void setReqName(String reqName) {
+		this.reqName = reqName;
+	}
+	public boolean isSuccess() {
+		return isSuccess;
+	}
+	public void setSuccess(boolean isSuccess) {
+		this.isSuccess = isSuccess;
+	}
+	public String getResRedirectUrl() {
+		return resRedirectUrl;
+	}
+	public void setResRedirectUrl(String resRedirectUrl) {
+		this.resRedirectUrl = resRedirectUrl;
+	}
 
 }
