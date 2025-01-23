814d2e4f5c62772b84b2b2909939bb54751dfecf
jwjeong <timestar7979@gmail.com>
2025-01-24 00:37:41 +0900

    feat: 근태 기준 관리 Controller 수정

--- erp-spring/src/main/java/com/erp/controller/CriterionAttController.java
+++ erp-spring/src/main/java/com/erp/controller/CriterionAttController.java
@@ -1,80 +1,70 @@
 package com.erp.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.erp.dto.CriterionDto;
+import com.erp.dto.CriAttDelDto;
+import com.erp.dto.CriAttDto;
+import com.erp.dto.CriAttRegDto;
+import com.erp.dto.CriAttUpdDto;
+import com.erp.service.CriterionAttService;
 
 @Controller
 public class CriterionAttController {
 	
-	public List<CriterionDto> list = new ArrayList<>();
+private static final Logger logger = LogManager.getLogger(CriterionDepController.class);
 	
-	public CriterionAttController() {
-		list.add(new CriterionDto("B0001", "INNER", "Y", "출근", "Y"));
-		list.add(new CriterionDto("B0002", "INNER", "Y", "지각", "Y"));
-		list.add(new CriterionDto("B0003", "INNER", "N", "결근", "Y"));
-		list.add(new CriterionDto("B0004", "OUTER", "N", "외근", "Y"));
-		list.add(new CriterionDto("B0005", "OUTER", "N", "출장", "Y"));
-		list.add(new CriterionDto("B0006", "INNER", "N", "연장", "Y"));
-		list.add(new CriterionDto("B0007", "INNER", "N", "휴가", "Y"));
-	}
+	@Inject
+	private CriterionAttService service;
 	
 	@RequestMapping("/criterionAttendance.do")
-	public String criterionAttendance(Model model) {
-		model.addAttribute("list", list);
+	public String criterionAttendance(Model model) throws Exception {
+		CriAttDto dtoReq = new CriAttDto();
+		CriAttDto dtoRes = service.criterionAttendance(dtoReq);
+		
+		model.addAttribute("list", dtoRes.getResList());
 		return "criterion_attendance";
 	}
 	
 	@PostMapping("/criterionAttendanceRegister.do")
-	public String criterionAttendanceRegister(HttpServletRequest request) {
-		String code = request.getParameter("code");
-		String type = request.getParameter("type");
-		String status = request.getParameter("status");
-		String name = request.getParameter("name");
+	public String criterionAttendanceRegister(HttpServletRequest request) throws Exception {
+		CriAttRegDto dtoReq = new CriAttRegDto();
+		dtoReq.setReqCode(request.getParameter("code"));
+		dtoReq.setReqType(request.getParameter("type"));
+		dtoReq.setReqStatus(request.getParameter("status").charAt(0));
+		dtoReq.setReqName(request.getParameter("name"));
 		
-		CriterionDto criterionDto = new CriterionDto(code, type, status, name, "Y");
-		list.add(criterionDto);
+		CriAttRegDto dtoRes = service.criterionAttendanceRegister(dtoReq);
 		
-		return "redirect:/criterionAttendance.do";
+		return dtoRes.getResRedirectUrl();
 	}
 	
 	@PostMapping("/criterionAttendanceUpdate.do")
-	public String criterionAttendanceUpdate(HttpServletRequest request) {
-		String code = request.getParameter("code");
-		String type = request.getParameter("type");
-		String status = request.getParameter("status");
-		String name = request.getParameter("name");
+	public String criterionAttendanceUpdate(HttpServletRequest request) throws Exception {
+		CriAttUpdDto dtoReq = new CriAttUpdDto();
+		dtoReq.setReqCode(request.getParameter("code"));
+		dtoReq.setReqType(request.getParameter("type"));
+		dtoReq.setReqStatus(request.getParameter("status").charAt(0));
+		dtoReq.setReqName(request.getParameter("name"));
+		CriAttUpdDto dtoRes = service.criterionAttendanceUpdate(dtoReq);
 		
-		list = list.stream().map(dto -> {
-			if (dto.getCode().equals(code)) {
-				dto.setType(type);
-				dto.setStatus(status);
-				dto.setName(name);
-				return dto;
-			}
-			return dto;
-		}).collect(Collectors.toList());
-		
-		return "redirect:/criterionAttendance.do";
+		return dtoRes.getResRedirectUrl();
 	}
 	
 	@PostMapping("/criterionAttendanceDelete.do")
-	public String criterionAttendanceDelete(HttpServletRequest request) {
-		String code = request.getParameter("code");
+	public String criterionAttendanceDelete(HttpServletRequest request) throws Exception {
+		CriAttDelDto dtoReq = new CriAttDelDto();
+		dtoReq.setReqCode(request.getParameter("code"));
+		CriAttDelDto dtoRes = service.criterionAttendanceDelete(dtoReq);
 		
-		list = list.stream().filter(dto -> !dto.getCode().equals(code))
-				.collect(Collectors.toList());
-		
-		return "redirect:/criterionAttendance.do";
+		return dtoRes.getResRedirectUrl();
 	}
 
 }
