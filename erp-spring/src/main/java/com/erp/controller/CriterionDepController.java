61d8b55b0d4099e22fe7da20646a43dddcaa0411
jwjeong <timestar7979@gmail.com>
2025-01-23 23:51:49 +0900

    feat: Controller 수정

--- erp-spring/src/main/java/com/erp/controller/CriterionDepController.java
+++ erp-spring/src/main/java/com/erp/controller/CriterionDepController.java
@@ -1,76 +1,70 @@
 package com.erp.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.erp.dto.CriterionDto;
+import com.erp.dto.CriDepDelDto;
+import com.erp.dto.CriDepDto;
+import com.erp.dto.CriDepRegDto;
+import com.erp.dto.CriDepUpdDto;
+import com.erp.service.CriterionDepService;
 
 @Controller
 public class CriterionDepController {
 	
-	public List<CriterionDto> list = new ArrayList<>();
+	private static final Logger logger = LogManager.getLogger(CriterionDepController.class);
 	
-	public CriterionDepController() {
-		list.add(new CriterionDto("A0001", "MANAGE", "Y", "경영팀", "Y"));
-		list.add(new CriterionDto("A0002", "HR", "Y", "인사팀", "Y"));
-		list.add(new CriterionDto("A0003", "R&D", "N", "연구팀", "Y"));
-	}
+	@Inject
+	private CriterionDepService service;
 	
 	@RequestMapping("/criterionDepartment.do")
-	public String criterionDepartment(HttpServletRequest request, Model model) {
-		model.addAttribute("list", list);
+	public String criterionDepartment(HttpServletRequest request, Model model) throws Exception {
+		CriDepDto dtoReq = new CriDepDto();
+		CriDepDto dtoRes = service.criterionDepartment(dtoReq);
+		
+		model.addAttribute("list", dtoRes.getResList());
 		return "criterion_department";
 	}
 	
 	@PostMapping("/criterionDepartmentRegister.do")
-	public String criterionDepartmentRegister(HttpServletRequest request) {
-		String code = request.getParameter("code");
-		String type = request.getParameter("type");
-		String status = request.getParameter("status");
-		String name = request.getParameter("name");
+	public String criterionDepartmentRegister(HttpServletRequest request) throws Exception {
+		CriDepRegDto dtoReq = new CriDepRegDto();
+		dtoReq.setReqCode(request.getParameter("code"));
+		dtoReq.setReqType(request.getParameter("type"));
+		dtoReq.setReqStatus(request.getParameter("status").charAt(0));
+		dtoReq.setReqName(request.getParameter("name"));
 		
-		CriterionDto criterionDto = new CriterionDto(code, type, status, name, "Y");
-		list.add(criterionDto);
+		CriDepRegDto dtoRes = service.criterionDepartmentRegister(dtoReq);
 		
-		return "redirect:/criterionDepartment.do";
+		return dtoRes.getResRedirectUrl();
 	}
 	
 	@PostMapping("/criterionDepartmentUpdate.do")
-	public String criterionDepartmentUpdate(HttpServletRequest request) {
-		String code = request.getParameter("code");
-		String type = request.getParameter("type");
-		String status = request.getParameter("status");
-		String name = request.getParameter("name");
+	public String criterionDepartmentUpdate(HttpServletRequest request) throws Exception {
+		CriDepUpdDto dtoReq = new CriDepUpdDto();
+		dtoReq.setReqCode(request.getParameter("code"));
+		dtoReq.setReqType(request.getParameter("type"));
+		dtoReq.setReqStatus(request.getParameter("status").charAt(0));
+		dtoReq.setReqName(request.getParameter("name"));
+		CriDepUpdDto dtoRes = service.criterionDepartmentUpdate(dtoReq);
 		
-		list = list.stream().map(dto -> {
-			if (dto.getCode().equals(code)) {
-				dto.setType(type);
-				dto.setStatus(status);
-				dto.setName(name);
-				return dto;
-			}
-			return dto;
-		}).collect(Collectors.toList());
-		
-		return "redirect:/criterionDepartment.do";
+		return dtoRes.getResRedirectUrl();
 	}
 	
 	@PostMapping("/criterionDepartmentDelete.do")
-	public String criterionDepartmentDelete(HttpServletRequest request) {
-		String code = request.getParameter("code");
+	public String criterionDepartmentDelete(HttpServletRequest request) throws Exception {
+		CriDepDelDto dtoReq = new CriDepDelDto();
+		dtoReq.setReqCode(request.getParameter("code"));
+		CriDepDelDto dtoRes = service.criterionDepartmentDelete(dtoReq);
 		
-		list = list.stream().filter(dto -> !dto.getCode().equals(code))
-				.collect(Collectors.toList());
-		
-		return "redirect:/criterionDepartment.do";
+		return dtoRes.getResRedirectUrl();
 	}
 	
 }
