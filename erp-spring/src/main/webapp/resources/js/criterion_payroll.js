231eb27f9c72c8a7c5d545735227cba641bc5f9a
jwjeong <timestar7979@gmail.com>
2025-01-26 19:36:40 +0900

    feat: js 일괄 수정
    - 기준 관리 다이얼로그 화면 수정
    - 코드, 유형 입력 disabled 처리

--- erp-spring/src/main/webapp/resources/js/criterion_payroll.js
+++ erp-spring/src/main/webapp/resources/js/criterion_payroll.js
@@ -3,11 +3,18 @@ const dialog = document.querySelector('dialog');
 window.onClickItem = function onClickItem(item) {
 	console.log(item);
 	dialog.showModal();
+	
+	const iic = document.getElementById('info-input-code');
+	const iit =	document.getElementById('info-input-type');
+	
 	document.getElementById('info-input-id').value = item.id;
-	document.getElementById('info-input-code').value = item.code;
-	document.getElementById('info-input-type').value = item.type;
+	iic.value = item.code;
+	iit.value = item.type;
 	document.getElementById('info-input-status').value = item.status;
 	document.getElementById('info-input-name').value = item.name;
+	
+	iic.disabled = true;
+	iit.disabled = true;
 }
 window.onClickClose = function () {
 	dialog.close();
