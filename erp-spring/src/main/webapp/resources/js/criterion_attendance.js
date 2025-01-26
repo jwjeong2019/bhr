b13d5c07007436bb9e7f421260985746e8d60f16
jwjeong <timestar7979@gmail.com>
2025-01-26 19:03:23 +0900

    feat: 근태 기준 관리 화면 수정
    - js 모듈 분리
    - 수정, 삭제 시 code에서 id로 처리하도록 수정

--- erp-spring/src/main/webapp/resources/js/criterion_attendance.js
+++ erp-spring/src/main/webapp/resources/js/criterion_attendance.js
@@ -1,14 +1,24 @@
-// ========== business ==========
-// variables
-const storage = getStorage();
-// functions
-function getStorage() {
-    return {
-        user: {
-            id: localStorage.getItem('id'),
-            email: localStorage.getItem('email'),
-            name: localStorage.getItem('name'),
-            role: localStorage.getItem('role')
-        }
-    };
+const infoForm = document.getElementById('info-form');
+const dialog = document.querySelector('dialog');
+window.onClickItem = function (item) {
+	console.log(item);
+	dialog.showModal();
+	document.getElementById('info-input-id').value = item.id;
+	document.getElementById('info-input-code').value = item.code;
+	document.getElementById('info-input-type').value = item.type;
+	document.getElementById('info-input-status').value = item.status;
+	document.getElementById('info-input-name').value = item.name;
+}
+window.onClickClose = function () {
+	dialog.close();
+}
+window.onClickUpdate = function () {
+	infoForm.action = 'criterionAttendanceUpdate.do';
+	infoForm.method = 'post';
+	infoForm.submit();
+}
+window.onClickDelete = function () {
+	infoForm.action = 'criterionAttendanceDelete.do';
+	infoForm.method = 'post';
+	infoForm.submit();
 }
