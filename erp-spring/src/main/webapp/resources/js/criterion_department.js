0b58da525ada298403225e03ca368c91c7ff3c4e
jwjeong <timestar7979@gmail.com>
2025-01-26 19:25:44 +0900

    feat: 부서 기준 관리 화면 수정
    - js 모듈 분리
    - id 파라미터 설정

--- erp-spring/src/main/webapp/resources/js/criterion_department.js
+++ erp-spring/src/main/webapp/resources/js/criterion_department.js
@@ -1,71 +1,24 @@
-// imports
-// import UlList from './component/crt_dep_ul_list.js';
-
-// ========== business ==========
-// variables
-const storage = getStorage();
-// functions
-function getStorage() {
-    return {
-        user: {
-            id: localStorage.getItem('id'),
-            email: localStorage.getItem('email'),
-            name: localStorage.getItem('name'),
-            role: localStorage.getItem('role')
-        }
-    };
+const infoForm = document.getElementById('info-form');
+const dialog = document.querySelector('dialog');
+window.onClickItem = function (item) {
+	console.log(item);
+	dialog.showModal();
+	document.getElementById('info-input-id').value = item.id;
+	document.getElementById('info-input-code').value = item.code;
+	document.getElementById('info-input-type').value = item.type;
+	document.getElementById('info-input-status').value = item.status;
+	document.getElementById('info-input-name').value = item.name;
 }
-
-// ========== view ==========
-// variables
-let list = [];
-let code = '';
-let type = '';
-let status = '';
-let name = '';
-let updCode = '';
-let updType = '';
-let updStatus = '';
-let updName = '';
-// executions
-init();
-// events
-document.getElementById('register-input-code').onchange = setCode;
-document.getElementById('register-input-type').onchange = setType;
-document.getElementById('register-input-status').onchange = setStatus;
-document.getElementById('register-input-name').onchange = setName;
-document.getElementById('btn-register').onclick = register;
-document.getElementById('info-input-code').onchange = setUpdCode;
-document.getElementById('info-input-type').onchange = setUpdType;
-document.getElementById('info-input-status').onchange = setUpdStatus;
-document.getElementById('info-input-name').onchange = setUpdName;
-// functions
-function init() {
+window.onClickClose = function () {
+	dialog.close();
 }
-function setCode(e) {
-    code = e.target.value;
+window.onClickUpdate = function () {
+	infoForm.action = 'criterionDepartmentUpdate.do';
+	infoForm.method = 'post';
+	infoForm.submit();
 }
-function setType(e) {
-    type = e.target.value;
-}
-function setStatus(e) {
-    status = e.target.value;
-}
-function setName(e) {
-    name = e.target.value;
-}
-function register(e) {
-    list.push({ code, type, status, name });
-}
-function setUpdCode(e) {
-    updCode = e.target.value;
-}
-function setUpdType(e) {
-    updType = e.target.value;
-}
-function setUpdStatus(e) {
-    updStatus = e.target.value;
-}
-function setUpdName(e) {
-    updName = e.target.value;
+window.onClickDelete = function () {
+	infoForm.action = 'criterionDepartmentDelete.do';
+	infoForm.method = 'post';
+	infoForm.submit();
 }
