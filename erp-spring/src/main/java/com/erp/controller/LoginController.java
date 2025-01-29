e748cf956da7977cb0ba5a20b22b0af5b0b8268f
jwjeong <timestar7979@gmail.com>
2025-01-30 01:16:29 +0900

    feat: 로그인 Controller 수정
    - /loginAction.do 맵핑 제거
    - /logoutAction.do 맵핑 제거

--- erp-spring/src/main/java/com/erp/controller/LoginController.java
+++ erp-spring/src/main/java/com/erp/controller/LoginController.java
@@ -31,36 +31,4 @@ public class LoginController {
 		model.addAttribute("msg", dtoRes.getResMsg());
 		return "login";
 	}
-	
-	@RequestMapping(path = "/loginAction.do", method = RequestMethod.POST)
-	public String loginAction(HttpServletRequest request, RedirectAttributes ra) throws Exception {
-		LoginActionDto dtoReq = new LoginActionDto();
-		dtoReq.setReqId(request.getParameter("id"));
-		dtoReq.setReqPwd(request.getParameter("pwd"));
-		LoginActionDto dtoRes = service.loginAction(dtoReq);
-		
-		if (dtoRes.isSuccess()) {
-			HttpSession session = request.getSession();
-			session.setAttribute("id", dtoRes.getResId());
-			session.setAttribute("email", dtoRes.getResEmail());
-			session.setAttribute("name", dtoRes.getResName());
-			session.setAttribute("role", dtoRes.getResRole());
-			return dtoRes.getResRedirectUrl();
-		}
-		
-		ra.addFlashAttribute("msg", "fail");
-		return dtoRes.getResRedirectUrl();
-	}
-	
-	@PostMapping("/logoutAction.do")
-	public String logoutAction(HttpServletRequest request, RedirectAttributes ra) {
-		HttpSession session = request.getSession();
-		session.removeAttribute("id");
-		session.removeAttribute("email");
-		session.removeAttribute("name");
-		session.removeAttribute("role");
-		
-		ra.addFlashAttribute("msg", "logout");
-		return "redirect:/login.do";
-	}
 }
