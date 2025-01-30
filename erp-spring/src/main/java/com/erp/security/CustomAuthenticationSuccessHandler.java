cd211c9632024178c3bef68842bc35d275e5ee5c
jwjeong <timestar7979@gmail.com>
2025-01-30 15:58:46 +0900

    feat: Spring Security Handler 수정
    - 로그인 유저 정보 세션 처리

--- erp-spring/src/main/java/com/erp/security/CustomAuthenticationSuccessHandler.java
+++ erp-spring/src/main/java/com/erp/security/CustomAuthenticationSuccessHandler.java
@@ -4,19 +4,35 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.erp.dao.AdminDao;
+import com.erp.dao.EmployeeDao;
+import com.erp.dto.AdminDto;
+import com.erp.dto.EmployeeDto;
+import com.erp.vo.AdminVO;
+import com.erp.vo.EmployeeVO;
 
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
 	private static final Logger logger = LogManager.getLogger(CustomAuthenticationSuccessHandler.class);
+	
+	@Inject
+	private AdminDao adminDao;
+	
+	@Inject
+	private EmployeeDao employeeDao;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -30,7 +46,31 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 				.peek(roleName -> logger.warn("roleName: {}", roleName))
 				.collect(Collectors.toList());
 		
-		response.sendRedirect("/main.do");
+		UserDetails details = (UserDetails) authentication.getPrincipal();
+		
+		HttpSession session = request.getSession();
+		
+		try {
+			AdminDto dtoAdm = new AdminDto();
+			dtoAdm.setEmail(details.getUsername());
+			AdminVO voAdm = adminDao.selectOne(dtoAdm);
+			if (voAdm != null) {
+				session.setAttribute("name", voAdm.getName());
+				response.sendRedirect("/main.do");
+				return;
+			}
+			
+			EmployeeDto dtoEmp = new EmployeeDto();
+			dtoEmp.setEmail(details.getUsername());
+			EmployeeVO voEmp = employeeDao.selectOne(dtoEmp);
+			if (voEmp != null) {
+				session.setAttribute("name", voEmp.getName());
+				response.sendRedirect("/main.do");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
